a3e8d27c407f5df42736175efb77bf8971769949
LoppA <Lopacheco@outlook.com>
2015-08-18 09:43:43 -0300

    Added condition and multi-line string

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -1,1 +1,22 @@
+
 print('Hello World')
+
+salse = 'tomato'
+
+#string multiline
+order_description = """
+The client asked for pizza with less cheese.
+Instead of cheese he would like to have more ham.
+"""
+
+order_number = 42
+
+order_number = 'other thing'
+
+if salse.startswith('toma'):
+    print("it's probably tomato...")
+else:
+    print("not tomato")
+
+if order_description:
+    print(order_description)
