6d003374c2c0aca41973c87e23839eacd3590cfa
LoppA <Lopacheco@outlook.com>
2015-08-18 11:00:50 -0300

    Cleared format string

--- pyexamples/loops.py
+++ pyexamples/loops.py
@@ -11,5 +11,14 @@ pedidos = [
     }
 ]
 
+
 for pedido in pedidos:
     print('Nome: {0}\nSabor: {1}' .format(pedido['nome'],pedido['sabor']))
+
+#printa na ordem q ta
+for pedido in pedidos:
+    print('Nome: {}\nSabor: {}' .format(pedido['nome'],pedido['sabor']))
+
+for pedido in pedidos:
+    s = 'Nome: {}\nSabor: {}'
+    print(s.format(pedido['nome'],pedido['sabor']))
